a784d8e71d8b3daac4e6b5bddd604a690cfcf699
dogdark <42787966+a8dogdark@users.noreply.github.com>
2024-05-26 23:09:13 -0400

    Update lamp.sh

--- lamp/lamp.sh
+++ lamp/lamp.sh
@@ -1,1 +1,10 @@
+#! /bin/bash
+
+# Confirmamos que el sistema sea de 64bits
+# We confirm that the system is 64-bit
+is64bit=$(getconf LONG_BIT)
+if [ "${is64bit}" != '64' ]; then
+    echo "El sistema solo debe ser de 64 bits"
+    exit 1
+fi
 
