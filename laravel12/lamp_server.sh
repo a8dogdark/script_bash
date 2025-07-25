4c372bdfc937351e1db967d97d9afd924bbc4a12
dogdark <42787966+a8dogdark@users.noreply.github.com>
2025-07-25 10:34:43 -0400

    Update lamp_server.sh

--- laravel12/lamp_server.sh
+++ laravel12/lamp_server.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 
 // validaciones
+
+//el sistema solo soporta ubuntu  derivados
+
+//solo se permite sistema de 64 bits
 is64bit=$(getconf LONG_BIT)
 if [ "${is64bit}" != '64' ]; then
     echo "El sistema solo debe ser de 64 bits"
     exit 1
 fi
 
+//validamos si es centos o almalinux, si es así no se instala
 if [ -f "/etc/redhat-release" ]; then
     Centos6Check=$(cat /etc/redhat-release | grep ' 6.' | grep -iE 'centos|Red Hat')
     if [ "${Centos6Check}" ]; then
@@ -15,8 +20,14 @@ if [ -f "/etc/redhat-release" ]; then
     fi
 fi
 
+//verificamos que el usuario sea root
 if [ "$(id -u)" -eq 0 ]; then
-  echo "El programa debes ejecutarlo como root"
+  echo "exito estas como root"
+  exit 1
 else
-  echo "El script NO se está ejecutando como root"
+  echo "El programa debes ejecutarlo como root"
+  echo "Para ingresar como root"
+  echo "UBUNTU -> sudo su -"
+  echo "DEBIAN -> su -"
+  exit 1
 fi
