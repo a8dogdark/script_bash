db7d8c01c4fff4a0c31e07ba55095521b6ed1f92
dogdark <42787966+a8dogdark@users.noreply.github.com>
2025-07-27 03:33:15 -0400

    Update lamp.sh

--- laravel12/lamp.sh
+++ laravel12/lamp.sh
@@ -3,8 +3,6 @@
 echo -e "\e[?25l"
 temp="/tmp"
 NODE_VERSION=24
-#sudo apt install zenity -y
-
 progress(){
     (
     	echo ${porcentaje}
@@ -65,6 +63,7 @@ if [ -f "/etc/os-release" ]; then
         DISTRO="DEBIAN"
         MYSQL="mariadb-server"
     fi
+    
 else 
     echo "Hay error al encontrar la distribuición, no la reconoce el programa"
     exit 1
@@ -188,44 +187,87 @@ mensaje="Preparando sistema para instalar lamp"
 porcentaje=0
 progress
 
+if ! dpkg -l | grep -q "^ii curl " 2>/dev/null; then
+    mensaje="Instalando Curl"
+    porcentaje=2
+    progress
+    sudo apt install -y -q curl > /dev/null 2>&1
+fi
+
+if ! dpkg -l | grep -q "^ii unzip " 2>/dev/null; then
+    mensaje="Instalando Unzip"
+    porcentaje=3
+    progress
+    sudo apt install -y -q unzip > /dev/null 2>&1
+fi
+
+if ! dpkg -l | grep -q "^ii wget " 2>/dev/null; then
+    mensaje="Instalando Wget"
+    porcentaje=4
+    progress
+    sudo apt install -y -q wget > /dev/null 2>&1
+fi
+
+if ! dpkg -l | grep -q "^ii git " 2>/dev/null; then
+    mensaje="Instalando Git"
+    porcentaje=5
+    progress
+    sudo apt install -y -q git > /dev/null 2>&1
+fi
+
+if ! dpkg -l | grep -q "^ii sed " 2>/dev/null; then
+    mensaje="Instalando Sed"
+    porcentaje=6
+    progress
+    sudo apt install -y -q sed > /dev/null 2>&1
+fi
+
+if ! dpkg -l | grep -q "^ii debconf-utils " 2>/dev/null; then
+    mensaje="Instalando Debconf-utils"
+    porcentaje=7
+    progress
+    sudo apt install -y -q debconf-utils > /dev/null 2>&1
+fi
+
 clear
 
 #Validamos si existen las app agregadas
 if [ "${DISTRO}" = "UBUNTU" ]; then
     # Si NO está el PPA de Ondřej, lo añadimos e instalamos
-    if ! grep -q "ppa.launchpadcontent.net/ondrej/php" /etc/apt/sources.list /etc/apt/sources.list.d/*.list 2>/dev/null; then
+    if ! grep -q "^deb .*ondrej/php" /etc/apt/sources.list /etc/apt/sources.list.d/*.list 2>/dev/null && \
+       ! sudo apt-add-repository --list | grep -q "ondrej/php" 2>/dev/null; then
         # Ejecución completamente silenciosa para Ubuntu
         mensaje="Actualizando sistema"
-        porcentaje=1
+        porcentaje=8
         progress
-        sudo apt update > /dev/null 2>&1
+        sudo apt update -y -q > /dev/null 2>&1
         mensaje="Agregando certificados"
-        porcentaje=2
+        porcentaje=9
         progress
-        sudo apt install -y apt-transport-https ca-certificates curl gnupg software-properties-common > /dev/null 2>&1
+        sudo apt install -y -q apt-transport-https ca-certificates curl gnupg software-properties-common > /dev/null 2>&1
         mensaje="Agregando repositorio Ondrej"
-        porcentaje=3
+        porcentaje=10
         progress
-        sudo add-apt-repository ppa:ondrej/php -y > /dev/null 2>&1
+        sudo add-apt-repository ppa:ondrej/php -y -q > /dev/null 2>&1
     fi
 elif [ "${DISTRO}" = "DEBIAN" ]; then
     # Si NO está el repositorio directo de Ondřej, lo añadimos e instalamos
     if ! grep -q "packages.sury.org/php" /etc/apt/sources.list /etc/apt/sources.list.d/*.list 2>/dev/null; then
         # Ejecución completamente silenciosa para Debian
         mensaje="Actualizando sistema"
-        porcentaje=1
+        porcentaje=8
         progress
-        sudo apt update > /dev/null 2>&1
+        sudo apt update -y -q > /dev/null 2>&1
         mensaje="Agregando certificados"
-        porcentaje=2
+        porcentaje=9
         progress
-        sudo apt install -y apt-transport-https ca-certificates curl gnupg software-properties-common > /dev/null 2>&1
+        sudo apt install -y -q apt-transport-https ca-certificates curl gnupg software-properties-common > /dev/null 2>&1
         mensaje="Agregando Gpg"
-        porcentaje=3
+        porcentaje=10
         progress
         sudo curl -sSL https://packages.sury.org/php/apt.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/deb.sury.org-php.gpg >/dev/null
         mensaje="Agregando App Ondrej"
-        porcentaje=4
+        porcentaje=11
         progress
         echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list >/dev/null
     fi
@@ -238,7 +280,7 @@ if [ "${DISTRO}" = "UBUNTU" ] || [ "${DISTRO}" = "DEBIAN" ]; then
     # Verifica si el repositorio de NodeSource para la versión 24 NO está presente
     if ! grep -q "$NODE_REPO_PATTERN" /etc/apt/sources.list /etc/apt/sources.list.d/*.list 2>/dev/null; then
     	mensaje="Agregando App NodeJs ${NODE_VERSION}"
-        porcentaje=5
+        porcentaje=12
         progress
         curl -fsSL "[https://deb.nodesource.com/setup_$](https://deb.nodesource.com/setup_$){NODE_VERSION}.x" | sudo -E bash - > /dev/null 2>&1 
     fi
@@ -260,32 +302,237 @@ if [ -n "$programas" ] && [ "$status" -eq 0 ]; then
         case "$tag_seleccionado" in
             1)
                 # Visual Studio Code
-                echo "eligió Visual"
+                mensaje="Agregando app Visual studio code"
+                porcentaje=13
+                progress
+                sudo apt-get install -y -q wget gpg apt-transport-https > /dev/null 2>&1 
+                wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg > /dev/null 2>&1 
+                sudo install -y -q -D -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg > /dev/null 2>&1 
+                rm -f microsoft.gpg > /dev/null 2>&1 
+                touch /etc/apt/sources.list.d/vscode.sources
+                sudo echo 'Types: deb\nURIs: https://packages.microsoft.com/repos/code\nSuites: stable\nComponents: main\nArchitectures: amd64,arm64,armhf\nSigned-By: /usr/share/keyrings/microsoft.gpg' >> /etc/apt/sources.list.d/vscode.sources
                 ;;
             2)
                 # Sublime Text
-                echo "eligio Sublime"
+                mensaje="Agregando app Sublime text"
+                porcentaje=14
+                progress
+                wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null 2>&1
+                echo -e 'Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc' | sudo tee /etc/apt/sources.list.d/sublime-text.sources > /dev/null 2>&1
                 ;;
             3)
                 # Brave Browser
-                echo "Eligio Brave"
+                mensaje="Agregando app Brave"
+                porcentaje=15
+                progress
+                sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg > /dev/null 2>&1
+                sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources > /dev/null 2>&1
                 ;;
             4)
                 # Google Chrome
-                echo "eligio Chrome"
+                mensaje="Google Chrome sin app"
+                porcentaje=16
+                progress
                 ;;
             *)
-                echo "Advertencia: Opción de software desconocida seleccionada: $tag_seleccionado"
+                mensaje="Error... Software no reconocido"
+                porcentaje=100
+                progress
+                sleep 3
+                clear 
+                exit 1
                 ;;
         esac
     done
 fi
 
+#instalamos lamp
+#instalando apache
+if ! dpkg -l | grep -q "^ii  apache2 " 2>/dev/null; then
+    mensaje="Instalando Apache"
+    porcentaje=20
+    progress
+    sudo apt install -y -q apache2 > /dev/null 2>&1
+fi
 
+mensaje="Habilitando url Dinamicas Apache"
+porcentaje=21
+progress
+sudo a2enmod rewrite
+sleep 1
+mensaje="Reiniciamos Apache"
+porcentaje=22
+progress
+sudo service apache2 restart
+sleep 1
+
+#instalando apache
+mensaje="Instalando Php 8.4"
+porcentaje=25
+progress
+if ! dpkg -l | grep -q "^ii  php8.4-cli " 2>/dev/null; then
+    sudo apt install -y -q php8.4 > /dev/null 2>&1
+    sleep 1s
+    mensaje="Instalando librerías Php 8.4"
+    porcentaje=30
+    progress
+    php8.4-{cli,xml,curl,mbstring,mysql,mysqlnd,mysqli,zip,common,opcache,phar,pdo,posix,readline,bz2,calendar,ctype,dom,exif,ffi,fileinfo,gd,gettext,iconv,mcrypt,shmop,simplexml,sockets,sysvmsg,tokenizer,xmlreader,xmlwriter,xsl,bcmath,json}
+fi
+
+#instalando apache
+mensaje="Instalando ${MYSQL}"
+porcentaje=40
+progress
+if ! dpkg -l | grep -q "^ii  ${MYSQL} " 2>/dev/null; then
+    sudo apt install -y -q ${MYSQL} > /dev/null 2>&1
+    mensaje="Agregamos la password a root"
+    porcentaje=45
+    progress
+    sleep 1s
+    DEFAULT_AUTH_PLUGIN=$(mysql -BN -e "SELECT @@default_authentication_plugin;" 2>/dev/null)
+    if [ -n "$DEFAULT_AUTH_PLUGIN" ]; then # Si pudimos obtener el valor
+        if [[ "$DEFAULT_AUTH_PLUGIN" == "mysql_native_password" ]]; then
+            mysql --execute="ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${passroot}';" >>/dev/null 2>&1
+            sudo mysql --execute="FLUSH PRIVILEGES;"
+        else
+            mysql --execute="ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '${passroot}';" >>/dev/null 2>&1
+            sudo mysql --execute="FLUSH PRIVILEGES;"
+        fi
+    else
+        clear
+        echo "No se pudo conectar a MySQL o determinar el plugin de autenticación por defecto."
+        echo "Asegúrate de que MySQL Server esté en ejecución y que tengas permisos para consultarlo."
+        exit 1
+    fi
+fi
+#instalamos phpmyadmin
+mensaje="Instalando phpmyadmin"
+porcentaje=50
+progress
+if ! dpkg -l | grep -q "^ii  phpmyadmin " 2>/dev/null; then
+    echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | sudo debconf-set-selections >>/dev/null
+    echo "phpmyadmin phpmyadmin/app-password-conf password ${passmyadmin}" | sudo debconf-set-selections
+    # Opcional: Solo si el instalador necesita la contraseña de root de MySQL para configurar la DB. Si ya tienes un usuario 'phpmyadmin' creado en MySQL, esto no es necesario.
+    echo "phpmyadmin phpmyadmin/mysql/admin-pass password ${passwordroot}" | sudo debconf-set-selections 
+    echo "phpmyadmin phpmyadmin/mysql/app-pass password ${passmyadmin}" | sudo debconf-set-selections
+    echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | sudo debconf-set-selections
+    # Para sistemas más antiguos, asegura que se haga el setup common
+    echo "phpmyadmin phpmyadmin/setup-common boolean true" | sudo debconf-set-selections
+    sleep 1s
+    mensaje="Actualizando el índice de paquetes"
+    porcentaje=52
+    progress
+    sudo apt update -y -q > /dev/null 2>&1
+    mensaje="Agregando phpMyAdmin"
+    porcentaje=54
+    progress
+    sudo apt install -y -q phpmyadmin > /dev/null 2>&1
+
+    if [ ! -f "/etc/apache2/conf-available/phpmyadmin.conf" ]; then
+        mensaje="Configurando phpMyAdmin"
+        porcentaje=56
+        progress
+        sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
+    fi
+    mensaje="Reiniciando Apache"
+    porcentaje=58
+    progress
+    sleep 1s
+    sudo systemctl reload apache2 > /dev/null 2>&1
+fi
+#instalamos composer
+if ! command -v composer >/dev/null 2>&1; then
+    mensaje="Instalando Composer"
+    porcentaje=60
+    progress
+    curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+    sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer > /dev/null 2>&1
+    rm /tmp/composer-setup.php 2>/dev/null
+    sudo echo 'export PATH="~/.config/composer/vendor/bin:$PATH"' >> ~/.bashrc
+    source ~/.bashrc
+fi
+#instalamos node
+if ! command -v nodejs >/dev/null 2>&1 || [[ "$(nodejs -v 2>/dev/null | sed -E 's/^v([0-9]+)\..*/\1/')" != "24" ]]; then
+    mensaje="Instalando Node24"
+    porcentaje=70
+    progress
+    sudo apt-get install -y -q nodejs > /dev/null 2>&1
+fi
+#instalamos los softwares adicionales
+#validamos si algun software si instalara, agregaremos las app
+if [ -n "$programas" ] && [ "$status" -eq 0 ]; then
+    IFS=' ' read -r -a programas_array <<< "$(echo "$programas" | tr -d '"')"
+    nombres_para_resumen=""
+    for tag in "${programas_array[@]}"; do
+        if [ -z "$nombres_para_resumen" ]; then
+            nombres_para_resumen="${NOMBRES_PROGRAMAS_MAP[$tag]}"
+        else
+            nombres_para_resumen+=", ${NOMBRES_PROGRAMAS_MAP[$tag]}"
+        fi
+    done
+    for tag_seleccionado in "${programas_array[@]}"; do
+        case "$tag_seleccionado" in
+            1)
+                # Visual Studio Code
+                mensaje="Instalando Visual studio code"
+                porcentaje=80
+                progress
+                sudo apt install -y -q code > /dev/null 2>&1
+                ;;
+            2)
+                # Sublime Text
+                mensaje="Instalando Sublime text"
+                porcentaje=83
+                progress
+                sudo apt-get install -y -q sublime-text > /dev/null 2>&1
+                ;;
+            3)
+                # Brave Browser
+                mensaje="Agregando app Brave"
+                porcentaje=86
+                progress
+                sudo apt install -y -q brave-browser > /dev/null 2>&1
+                ;;
+            4)
+                # Google Chrome
+                mensaje="Google Chrome sin app"
+                porcentaje=89
+                progress
+                wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+                sudo dpkg -i google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+                sudo apt --fix-broken install > /dev/null 2>&1
+                ;;
+            *)
+                mensaje="Error... Software no reconocido"
+                porcentaje=100
+                progress
+                sleep 3
+                clear 
+                exit 1
+                ;;
+        esac
+    done
+fi
+#creamos el nuevo proyecto
+mensaje="Preparando el nuevo proyecto"
+porcentaje=90
+progress
+if [ ! -d "/var/www" ]; then
+    #La carpeta /var/www NO existe.
+    sudo mkdir -p /var/www > /dev/null 2>&1
+fi
+if [ ! -d "/var/www/laravel" ]; then
+    #La carpeta /var/www NO existe.
+    sudo mkdir -p /var/www/laravel > /dev/null 2>&1
+fi
+sudo chmod 777 -R /var/www > /dev/null 2>&1
+#creamos la carpeta de los proyectos laravel
+
+#terminamos la instalacion
 
 #cursor visible
 echo -e "\e[?25h"
-clear
+
 exit 1
 
 
