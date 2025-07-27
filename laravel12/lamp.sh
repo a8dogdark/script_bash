34f4a3ff202830fbf5595ef10fb58e13cb88f2cb
dogdark <42787966+a8dogdark@users.noreply.github.com>
2025-07-27 04:56:16 -0400

    Update lamp.sh

--- laravel12/lamp.sh
+++ laravel12/lamp.sh
@@ -1,9 +1,27 @@
-´#! /bin/bash
+#! /bin/bash
 apago_cursor()
 {
-
+  echo -e "\e[?25l"
 }
 enciendo_cursor)
 {
-
+  echo -e "\e[?25h"
 }
+apago_cursor
+clear
+printf "*************************\n"
+printf "* Instalador de lamp y  *\n"
+printf "* laravel 12 By Dogdark *\n"
+printf "*************************\n"
+sleep 1s
+
+enciendo_cursor
+clear
+read -p "Nombre del proyecto: " PROYECTO_LARAVEL
+if [ -z "$PROYECTO_LARAVEL" ]; then
+  echo "el campo no puede venir vacio"
+fi
+read -p "Password para root Mysql: " PASS_ROOT
+if [ -z "$PASS_ROOT" ]; then
+  echo "el campo no puede venir vacio"
+fi
