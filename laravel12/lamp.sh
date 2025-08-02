a751e571a42563b43ff869c869dbd5a32ba59bf8
dogdark <42787966+a8dogdark@users.noreply.github.com>
2025-08-02 18:01:24 -0400

    Update lamp.sh

--- laravel12/lamp.sh
+++ laravel12/lamp.sh
@@ -125,3 +125,18 @@ if [[ $? -ne 0 ]]; then
   clear
   exit 1
 fi
+
+# Cuadro para elegir varios softwares
+SOFTWARES=$(dialog --title "Seleccionar softwares" \
+  --checklist "Seleccione los softwares que desea instalar (barra espacio para marcar):" 15 60 5 \
+  "brave" "Brave" off \
+  "code" "Visual Studio Code" off \
+  "ftpzilla" "Ftpzilla" off \
+  "google-chrome" "Google Chrome" off \
+  3>&1 1>&2 2>&3)
+
+if [[ $? -ne 0 ]]; then
+  dialog --title "Operación cancelada" --msgbox "Ha cancelado la operación. El instalador se cerrará." 7 50
+  clear
+  exit 1
+fi
