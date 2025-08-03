c13790e84b50860a609b81caf3b369ea8a1e3f15
dogdark <42787966+a8dogdark@users.noreply.github.com>
2025-08-03 00:09:41 -0400

    Create install.sh

---
+++ laravel12/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#####################################
+# Instalador de servidor Lamp para  #
+# Laravel 12 y creador de proyectos #
+#####################################
+
+# se debe instalar como usuario root 
+if [ "$EUID" -ne 0 ]; then
+  echo "Este script debe ser ejecutado como root."
+  exit 1
+fi
+# validamos que sea de 64 bits
+if [ "$(uname -m)" != "x86_64" ]; then
+  echo "Este script solo funciona en sistemas de 64 bits (x86_64)."
+  exit 1
+fi
+
+# Carga datos del sistema operativo
+. /etc/os-release
+
+DISTRO=$ID
+VERDISTRO=$VERSION_ID
+
+if ! { { [ "$DISTRO" = "ubuntu" ] && { [ "$VERDISTRO" = "22.04" ] || [ "$VERDISTRO" = "24.04" ]; }; } || \
+       { [ "$DISTRO" = "debian" ] && { [ "$VERDISTRO" = "11" ] || [ "$VERDISTRO" = "12" ]; }; } \
+     }; then
+  echo "Sistema no soportado. Solo Ubuntu 22.04, 24.04 o Debian 11, 12."
+  exit 1
+fi
